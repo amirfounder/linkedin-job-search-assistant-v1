bc0c629e677e49eb548776817ee804d9ea966207
Amir Sharapov <amir@amirfounder.com>
2022-06-02 17:26:30 -0500

    helpers. server sort of done. primarily helpers

--- src/helpers.py
+++ src/helpers.py
@@ -1,9 +1,10 @@
 import json
 from os import listdir
 from os.path import isfile
+from urllib.parse import unquote
 
 
-def get_json_from_directory(path):
+def get_json_from_directory(path: str):
     filenames = listdir(path)
     for filename in filenames:
         filepath = path + '/' + filename
@@ -28,6 +29,40 @@ def create_linkedin_search_queries():
     for company in get_companies():
         for template in get_linkedin_search_templates():
             yield template.format(company=company)
+
+
+def get_params_from_url(url: str):
+    params = [param.split('=') for param in url.split('?')[1].split('&')]
+    params = {k: v for k, v in params}
+    params['page'] = params.get('page', 0)
+    params['keywords'] = unquote(params.get('keywords')).replace(' ', '_')
+
+    return params
+
+
+def create_linkedin_search_results_html_filename(url: str, seconds_since_loaded: int):
+    params = get_params_from_url(url)
+
+    return 'type=search_results&keywords={}&page={}&seconds_since_loaded={}'.format(
+        params['keywords'],
+        params['page'],
+        seconds_since_loaded
+    )
+
+
+def create_linkedin_profile_html_filename(url: str, seconds_since_loaded: int):
+    profile_slug = url.split('linkedin.com/in/')[1]
+
+    return 'type=profile&profile_slug={}&seconds_since_loaded={}'.format(
+        profile_slug,
+        seconds_since_loaded
+    )
+
+
+def save_html_contents(html: str, filename: str):
+    path = 'data/scraped/' + filename.removeprefix('/')
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(html)
 
 
 for query in create_linkedin_search_queries():
--- src/server.py
+++ src/server.py
@@ -2,13 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 
 
-class Server:
-    def __init__(self):
-        self.app = Flask(__name__)
-        self.port = 8080
-        CORS(self.app)
+app = Flask(__name__)
+CORS(app)
 
-    def run(self):
-        self.app.run(port=self.port)
 
-    def
+def run():
+    app.run(
+        port=8080
+    )
